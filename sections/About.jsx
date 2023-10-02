cd89a831ae29c7d5dcc5a2ae3194838c8cd705bd
Vince Valmeo <vince.valmeo.dev@gmail.com>
2023-10-02 17:52:35 +0800

    Partial About

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,8 +1,16 @@
-'use client';
+"use client";
 
 const About = () => (
   <section>
-    About section
+    <p>About the Metaverse</p>
+    <p>
+      Metaverse is a new thing in the future, where you can enjoy the virtual
+      world by feeling like it's really real, you can feel what you feel in this
+      metaverse world, because this is really the madness of the metaverse of
+      today, using only VR devices you can easily explore the metaverse world
+      you want, turn your dreams into reality. Let's explore the madness of the
+      metaverse by scrolling down
+    </p>
   </section>
 );
 
