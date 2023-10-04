52355feeef9fd9e199be8b28c9d89cbcce35dbf6
Vince Valmeo <vince.valmeo.dev@gmail.com>
2023-10-04 18:34:28 +0800

    Complete World Section

---
+++ components/LocationPointer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const LocationPointer = () => {
+  return (
+    <h1>Locatio pointer</h1>
+    // <div
+    //   className={`w-[70px] h-[70px] p-[6px] rounded-full glassmorphism ${location}`}
+    // >
+    //   <img src={imgUrl} alt="people" className="w-full h-full" />
+    // </div>
+  );
+};
+
+export default LocationPointer;
--- sections/World.jsx
+++ sections/World.jsx
@@ -1,8 +1,57 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { TitleText, TypingText, LocationPointer } from "../components";
+import { fadeIn, staggerContainer } from "../utils/motion";
 
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings}`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`flex flex-col justify-center items-center`}
+    >
+      <TypingText title="|People on the World" />
+      <TitleText
+        title={
+          <>
+            Track friends around you and invite them to play together in the
+            same world
+          </>
+        }
+        textStyles="text-center max-w-4xl"
+      />
+
+      <motion.div
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        className="relative mt-[68px] flex w-full h-[550px]"
+      >
+        <img src="/map.png" alt="map" className="w-[1/2] h-full object-cover" />
+
+        <div className="absolute top-1 right-[5%] ">
+          <img src="people-card-2.png" alt="people" className="w-full h-full" />
+        </div>
+
+        <div className="absolute bottom-20 right-20">
+          <img src="people-1.png" alt="people" className="w-full h-full" />
+        </div>
+
+        <div className="absolute top-10 left-20">
+          <img src="people-2.png" alt="people" className="w-full h-full" />
+        </div>
+
+        <div className="absolute top-1/4 left-[5%] ">
+          <img src="people-card-1.png" alt="people" className="w-full h-full" />
+        </div>
+
+        <div className="absolute top-1/2 left-[40%] ">
+          <img src="people-3.png" alt="people" className="w-full h-full" />
+        </div>
+      </motion.div>
+    </motion.div>
   </section>
 );
 
