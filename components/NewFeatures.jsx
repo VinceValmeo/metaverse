88249b9127194a22f0c389a3a4247cde5d269cbb
Vince Valmeo <vince.valmeo.dev@gmail.com>
2023-10-04 14:47:27 +0800

    Complete WhatsNew Section

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,6 +1,14 @@
-const NewFeatures = () => (
-  <div>
-    New Features
+import styles from "../styles";
+
+const NewFeatures = ({ imgUrl, title, subtitle }) => (
+  <div className="flex gap-3 flex-col">
+    <div
+      className={`glassmorphism h-[50px] w-[50px] rounded-[15px] flex ${styles.flexCenter} items-start`}
+    >
+      <img src={imgUrl} />
+    </div>
+    <p className="text-white flex-1 font-semibold text-[24px]">{title}</p>
+    <p className="text-[#B0B0B0] flex-1 max-w-[400px]">{subtitle}</p>
   </div>
 );
 
--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,8 +1,52 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { newFeatures } from "../constants";
+import { NewFeatures, TitleText, TypingText } from "../components";
+import { staggerContainer, fadeIn, planetVariants } from "../utils/motion";
 
 const WhatsNew = () => (
-  <section>
-    What's new section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+    >
+      <motion.div
+        variants={fadeIn("right", "tween", 0.2, 1)}
+        className="flex-[0.75] flex justify-center flex-col"
+      >
+        <TypingText title="|Whats New?" />
+        <TitleText title={<>What's new about Metaversus?</>} />
+        <div className="mt-[31px] flex flex-col md:flex-row  gap-[24px]">
+          {newFeatures.map((feature, index) => (
+            <motion.div variants={fadeIn("right", "spring", index * 0.5, 0.5)}>
+              <NewFeatures
+                key={feature}
+                imgUrl={feature.imgUrl}
+                title={feature.title}
+                subtitle={feature.subtitle}
+              />
+            </motion.div>
+          ))}
+        </div>
+      </motion.div>
+
+      <motion.div
+        variants={planetVariants("right")}
+        className={`flex-1 ${styles.flexCenter}`}
+      >
+        <img
+          src="/whats-new.png"
+          alt="whats-new"
+          className="w-[90%] h-[90%] object-contain"
+        />
+      </motion.div>
+    </motion.div>
   </section>
 );
 
