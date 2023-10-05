923db616bde5d17fc4931b57e4a8a576b328e444
Vince Valmeo <vince.valmeo.dev@gmail.com>
2023-10-05 13:16:31 +0800

    Complete Insights section

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -1,9 +1,32 @@
-'use client';
-
-const InsightCard = () => (
-  <div>
-    Insight Card
-  </div>
+"use client";
+import { motion } from "framer-motion";
+import { fadeIn, staggerContainer } from "../utils/motion";
+const InsightCard = ({ imgUrl, title, subtitle, index }) => (
+  <motion.div
+    variants={fadeIn("up", "spring", index * 0.5, 1)}
+    className="flex md:flex-row flex-col gap-4"
+  >
+    <img
+      src={imgUrl}
+      className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
+      alt="world"
+    />
+    <div className="flex items-center">
+      <div className="flex-1 md:ml-[62px] flex flex-col max-w-[650px] text-white">
+        <h2 className="font-bold text-[30px]">{title}</h2>
+        <p className="mt-[16px] font-normal lg:text-[20px] text-[14px] text-secondary-white">
+          {subtitle}
+        </p>
+      </div>
+      <div className="lg:flex hidden items-center justify-center w-[100px] h-[100px] rounded-full bg-transparent border-[1px] border-white">
+        <img
+          src="/arrow.svg"
+          alt="arrow up"
+          className="w-[40%] h-[40%] object-contain"
+        />
+      </div>
+    </div>
+  </motion.div>
 );
 
 export default InsightCard;
--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,8 +1,33 @@
-'use client';
+"use client";
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { TitleText, TypingText, InsightCard } from "../components";
+import { fadeIn, staggerContainer } from "../utils/motion";
+import { insights } from "../constants";
 
 const Insights = () => (
-  <section>
-    Insights section
+  <section className={`${styles.paddings}`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`flex flex-col justify-center items-center`}
+    >
+      <TypingText title="|Insight" />
+      <TitleText
+        title={<>Insights About the Metaverse</>}
+        textStyles="text-center"
+      />
+
+      <div className="mt-10">
+        {insights.map((item, index) => (
+          <div className="mb-10">
+            <InsightCard imgUrl={item.imgUrl} {...item} index={index} />
+          </div>
+        ))}
+      </div>
+    </motion.div>
   </section>
 );
 
